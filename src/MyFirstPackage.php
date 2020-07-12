e736dca9f8ad5687e38268ac8d1797df3f520a4b
Ranjith Nair <ranjith1388@gmail.com>
2020-07-12 12:43:18 +0530

    Committed source code

---
+++ src/MyFirstPackage.php
@@ -0,0 +1,10 @@
+<?php
+
+     class MyFirstPackage{
+
+           namespace ranjith1388\MyFirstPackage
+           
+           public functio showMessage(){
+                  echo "This is my first package";
+           }
+     }
